7e8a42573fe5312980c2bddb59e5076f96331b85
L1ghtsitte <maxym105@gmail.com>
2026-05-18 18:19:53 +0300

    lesson_1

---
+++ lesson_3/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream> 
+
+int main()
+{
+//    if (УСЛОВИЕ)
+//    {
+//        // body if
+//    }
+
+    int if_a = 10;
+
+    if (if_a > 5)
+    {
+        std::cout << "if_a больше 5" << std::endl;
+    }
+
+    if (if_a > 20)
+    {
+        std::cout << "if_a больше 20" << std::endl;
+    }
+
+    bool if_b = true;
+
+    if (if_b)
+    {
+        std::cout << "if_b истина" << std::endl;
+    }
+
+    int if_c = 0; // false
+
+    if (if_c)
+    {
+        std::cout << "не выведется" << std::endl;
+    }
+
+    int if_d = 42;
+
+    if (if_d)
+    {
+        std::cout << "выведся, так как не 0" << std::endl;
+    }
+
+    int if_e = 15;
+
+    if (if_e > 10 && if_e < 20)
+    {
+        std::cout << "if_e в диапозоне 10-20" << std::endl;
+    }
+
+    if(if_e < 5 || if_e > 10)
+    {
+        std::cout << "if_e меньше 5 илил больше 10" << std::endl;
+    }
+
+    int if_f = 5;
+
+    if (if_f > 100)
+    {
+        std::cout << "положительное" << std::endl;
+        std::cout << "это уже не if" << std::endl;
+    }
+
+
+
+}
